62835c89d1daf6a26adf0b6dfc31142830cb3676
abdel <claudeotaku44@gmail.com>
2026-07-04 13:17:04 +0100

    sw install

--- js/app.js
+++ js/app.js
@@ -945,3 +945,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+          if  ("serviceWorker" in navigator ){
+      navigator.serviceWorker.register("/sw.js")
+    } 
